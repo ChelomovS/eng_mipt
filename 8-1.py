27ee4e5c3b163fe74f0caa95c98a14e34b7bfef3
Stepan Chelomov <142499379+ChelomovS@users.noreply.github.com>
2024-05-21 11:55:01 +0300

    Create 8-1.py

---
+++ 8-1.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+#Plot
+
+path1 = "./data.txt"
+path2 = "./settings.txt"
+file = "./8-1-plot.svg"
+xs = []
+ys = []
+
+with open(path2, "r") as settings_file:
+    settings_data = [float(num) for num in settings_file.read().split("\n")]
+
+time_step = settings_data[0]
+volt_step = settings_data[1]
+
+counter = 0
+with open(path1, "r") as f:
+    for line in f.readlines():
+        y = float(line)
+        ys.append(y*volt_step)
+        xs.append(float(counter*time_step))
+        counter = counter + 1
+
+xs = np.array(xs)
+ys = np.array(ys)
+
+_, ax = plt.subplots(figsize=(10, 10))
+ax.set_xlabel("Время, с", fontweight="bold")
+ax.set_ylabel("Напряжение, В", fontweight="bold")
+ax.set_title("Процесс зарядки и разрядки конденсатора в RC-цепи", fontweight="bold")
+
+my_plot = plt.plot(xs, ys, color='c', label="Зависимость U(t)",marker = 'o',
+                           markerfacecolor='tab:pink',
+                           markeredgecolor='m', markevery=10)
+
+ax.xaxis.set_major_locator(ticker.MaxNLocator(10))
+ax.xaxis.set_minor_locator(ticker.MaxNLocator(10))
+ax.yaxis.set_major_locator(ticker.MaxNLocator(10))
+ax.yaxis.set_minor_locator(ticker.MaxNLocator(10))
+
+plt.grid(color="pink", visible=True, which='major',axis='both',alpha=1, linestyle = ":")
+plt.grid(color="pink", visible=True, which='minor',axis='both',alpha=1, linestyle = ":")
+
+plt.legend()
+plt.show()
+plt.savefig(file)
